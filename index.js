aa4095903cbc8e46697da20d8dc6fe3cee30f254
Bahar Shafaei <bahar35shafaei@gmail.com>
2023-08-01 23:11:00 -0400

    add linter and prettier and assets

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const port = 3000;
 
-http.createServer((req, res) => {
-  if (req.url === '/') {
-    const filePath = path.join(__dirname, 'index.html');
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        res.writeHead(500);
-        res.end('Error loading index.html');
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-      }
-    });
-  }
-}).listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+http
+  .createServer((req, res) => {
+    if (req.url === "/") {
+      const filePath = path.join(__dirname, "index.html");
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          res.writeHead(500);
+          res.end("Error loading index.html");
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(data);
+        }
+      });
+    }
+  })
+  .listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
